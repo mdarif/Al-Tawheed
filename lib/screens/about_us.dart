24e4390f5d00f2742ab61018c68a3c38024bb29f
Mohammad Arif <arif_iq@yahoo.co.in>
2021-03-17 17:03:40 +0530

    Adding new screens & commented additional buttons on about us and contact us.

---
+++ lib/screens/about_us.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AboutUs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("About Us"),
+      ),
+      body: Center(
+        child: Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const ListTile(
+                leading: Icon(Icons.contact_mail),
+                title: Text('MUBA Dawah Club'),
+                subtitle: Text('We do give dawah to our brothers around!'),
+              ),
+              /*Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    child: const Text('BUY TICKETS'),
+                    onPressed: () {/* ... */},
+                  ),
+                  const SizedBox(width: 8),
+                  TextButton(
+                    child: const Text('LISTEN'),
+                    onPressed: () {/* ... */},
+                  ),
+                  const SizedBox(width: 8),
+                ],
+              ),*/
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/contact_us.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ContactUs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contact Us"),
+      ),
+      body: Center(
+        child: Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const ListTile(
+                leading: Icon(Icons.email),
+                title: Text('Please send an email to us at '),
+                subtitle: Text('arif.mohammed@gmail.com'),
+              ),
+              /*Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    child: const Text('BUY TICKETS'),
+                    onPressed: () {/* ... */},
+                  ),
+                  const SizedBox(width: 8),
+                  TextButton(
+                    child: const Text('LISTEN'),
+                    onPressed: () {/* ... */},
+                  ),
+                  const SizedBox(width: 8),
+                ],
+              ),*/
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
