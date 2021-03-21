373d6fafe276f595ad0509b44530ea35ac347e0b
Mohammad Arif <arif_iq@yahoo.co.in>
2021-03-21 21:53:03 +0530

    Added the Firebase Android support.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+//void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sharah Kitaab Al Tawheed',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.purple,
-      ),
-      home: HomeScreen(),
-    );
+        title: 'Sharah Kitaab Al Tawheed',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.purple,
+        ),
+        home: FutureBuilder(
+          future: _fbApp,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              print('You have an error! ${snapshot.error.toString()} ');
+              return Text('Something went wrong');
+            } else if (snapshot.hasData) {
+              return HomeScreen();
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        )
+        //HomeScreen(),
+        );
   }
 }
